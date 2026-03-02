4fa717964f96f7a1bf03932ced98b502e99d38c2
XuxadeBruxa <xenia94_06@yahoo.de>
2026-03-02 14:44:25 +0100

    header und footer erstellt und gestylt Bilder in Javascript eingefügt

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const popupImages = [
+    "./img/Lake with Montain.jpg",
+    "./img/Citylights.jpg",
+    "./img/Wolken.png",
+    "./img/Vogel.jpg",
+    "./img/Erde.jpg",
+    "./img/Mountain and Lake.jpg",
+    "./img/Duck.jpg",
+    "./img/Like a Dream.jpg",
+    "./img/Bird on a Stone.jpg",
+    "./img/Baby Gepard.jpg",
+    "./img/Mountain.jpg",
+    "./img/Winter.jpg",
+];
